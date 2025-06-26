235afaf1029bfec139f591716fc565fbb44c04f5
Mathew <mathewkariuki2005@gmail.com>
2025-06-26 16:46:04 +0300

    commented my index.js

--- index.js
+++ index.js
@@ -1,20 +1,25 @@
-const buttons = document.querySelectorAll("button[data-category]"); // FIXED (was only 1 button)
-const productSection = document.getElementById("products"); // FIXED: make sure variable matches function
+const buttons = document.querySelectorAll("button[data-category]"); // FIXED (was only 1 button) 
+const productSection = document.getElementById("products"); // FIXED: make sure variable matches function so that we dont have errors
 const url = "https://fakestoreapi.com/products";
 let allproducts = [];
-
+const m =document.getElementById("items");
+const z=document.getElementById('total');
+let cart=[]//here we are saying we have an array for cart but its = to 0 for now
 
 //step one is loading all the data when the document is opened by fetching data from the api
 document.addEventListener('DOMContentLoaded', function() {
-    fetch(url)
-    .then(res => res.json())
+    fetch(url)//we do this to fecth data in our api
+    .then(res => res.json())//this is like saying,hi..the api we have fetched let's make it to now be in json which is always in string form btw
     .then(data => {
+//now we haveour json in strng form..nice..so now whe we say .then(data=>{})we saying lets now do something with this data...which the api data...data here is just like 
+//a name...but its special cause anything thats named after converting our api details into json..it becomes the apis data
+//so here i said let that data to be = to allproducts so that i can access it later..
         allproducts = data;
         showProducts(data);
     });
 });
 
-//step two is creatin a function that shows all products
+//step two is creating a function that shows all products
 function showProducts(products){
     productSection.innerHTML='';
 
@@ -24,13 +29,14 @@ function showProducts(products){
         div.innerHTML=`
           <h3>${product.title}</h3>
             <p><strong>Category:</strong> ${product.category}</p>
-            <p><strong>Price:</strong> ${product.price}</p>`;
+            <p><strong>Price:</strong> ${product.price}</p>
+            <button data-id="${product.id}">Add to cart</button>`;
             productSection.appendChild(div);
     });
 }
 
 
-//assuming someone whats to see all products or men's wear they can just simply click the button and the data that is stored in our api will display
+//assuming someone wants to see all products or men's wear they can just simply click the button and the data that is stored in our api will display
 //depending on the button category clicked..this has to be in a loop so for it to work we wll use a foreach loop but there has to be an event listener
 //to listen for a button click
 
@@ -47,3 +53,5 @@ buttons.forEach(buttuon=>{
  }
  })
 })
+
+
